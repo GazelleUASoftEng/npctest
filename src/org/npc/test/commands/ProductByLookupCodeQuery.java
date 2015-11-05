05812ce020852fa2e9dfa18caa42a2ddee689673
Ryanjalbertson <ryanjalbertson@gmail.com>
2015-11-05 15:33:02 -0600

    Added some error checking for empty string and invalid lookupcode

--- src/org/npc/test/commands/ProductByLookupCodeQuery.java
+++ src/org/npc/test/commands/ProductByLookupCodeQuery.java
@@ -1,18 +1,29 @@
 package org.npc.test.commands;
 
 import org.gazelle.api.Product;
+import org.gazelle.enums.ProductApiRequestStatus;
 import org.gazelle.repositories.interfaces.ProductRepositoryInterface;
 
 public class ProductByLookupCodeQuery {
 
 	
 	public Product execute() {
+		if (lookupCode == "") {
+			return new Product().setApiRequestStatus(ProductApiRequestStatus.NOT_FOUND);
+		}
+		
+		if (this.productRepository.byLookupCode(this.lookupCode).equals(null)) {
+			return new Product().setApiRequestStatus(ProductApiRequestStatus.NOT_FOUND);
+		}
+		
 		return new Product(this.productRepository.byLookupCode(this.lookupCode));
 	}
 	
 	private String lookupCode;
 	
 	public ProductByLookupCodeQuery setLookupCode (String lookupCode) {
+		
+		
 		this.lookupCode = lookupCode;
 		return this;
 	}
